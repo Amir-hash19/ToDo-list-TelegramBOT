eddc42e5648f1eb06d98ad343564a6e17bb6821f
Amirhoseinheidari <amirhosein.hydri1381@gmail.com>
2025-12-02 22:39:36 +0330

    build: my task message_handler added

--- core/main.py
+++ core/main.py
@@ -1,5 +1,5 @@
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
-from database.base import engine, Base, get_db
+from database.base import engine, Base, get_db, SessionLocal
 from database.models import UserModel, TaskModel
 from telebot import TeleBot, apihelper, telebot
 from decouple import config
@@ -23,11 +23,11 @@ def start(message):
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 
     btn_register = KeyboardButton("Register")
-    btn_add = KeyboardButton("Add Task")
-    btn_list = KeyboardButton("My Tasks")
-    btn_Delete = KeyboardButton("Delete Task")
-    btn_about = KeyboardButton("About Us")
-    btn_contact = KeyboardButton("Contact Us")
+    btn_add = KeyboardButton("Add_Task")
+    btn_list = KeyboardButton("My_Tasks")
+    btn_Delete = KeyboardButton("Delete_Task")
+    btn_about = KeyboardButton("About_Us")
+    btn_contact = KeyboardButton("Contact_Us")
 
 
     keyboard.row(btn_register)
@@ -124,30 +124,87 @@ def ensure_registered(message, db):
     return user
 
 
-
-@bot.message_handler(commands=['Add'])
+@bot.message_handler(commands=['Add_Task'])
 def add_task(message):
-    db = next(get_db())
+    db = SessionLocal()  
 
-    user = ensure_registered(message, db)
-    if user is None:
+    try:
+        user = ensure_registered(message, db)
+        if user is None:
+            return
+
+        task_text = message.text.replace("/add", "").strip()
+        if not task_text:
+            bot.reply_to(message, "Please enter your task:")
+            return
+
+        new_task = TaskModel(
+            user_id=user.id,
+            title=task_text,
+            priority=3,
+            is_done=False
+        )
+        db.add(new_task)
+        db.commit()
+
+        bot.reply_to(message, f"Task '{task_text}' added successfully.")
+    finally:
+        db.close()
+
+
+
+
+bot.message_handler(lambda m: m.text == "My_Tasks")
+def my_tasks(message):
+    db = SessionLocal()
+    user_id = message.from_user.id
+
+    tasks = db.query(TaskModel).filter(TaskModel.user_id == user_id).all()
+    db.close()
+
+    if not tasks:
+        markup = ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(KeyboardButton("Add_Task"))
+        markup.add(KeyboardButton("Back"))
+
+        bot.send_message(
+            message.chat.id,
+            "You have no tasks yet.\nDo you want to add a new task?",
+            reply_markup=markup
+        )
         return 
-    
-    task_text = message.text.replace("/add", "").strip()
-    if not task_text:
-        bot.reply_to(message, "Please enter your task:")
-        return 
-    
-    new_task = TaskModel(
-        user_id=user.id,
-        title=task_text,
-        priority=3,
-        is_done=False
-    )
+    text = "Your Tasks:\n"
+    for t in tasks:
+        status = "✓" if t.is_done else "✗"
+        text += f"- {t.title} [{status}] (Priority: {t.priority})\n"
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(KeyboardButton("Add Task"))
+    markup.add(KeyboardButton("Back"))
+
+    bot.send_message(message.chat.id, text, reply_markup=markup)
+
+
+
+def save_task(message):
+    title = message.text
+    user_id = message.from_user.id
+
+    db = SessionLocal()
+    new_task = TaskModel(user_id=user_id, title=title)
     db.add(new_task)
     db.commit()
+    db.close()
 
-    bot.reply_to(message, f"Task {task_text} added successfully.")
+    bot.send_message(message.chat.id, "Task added successfully!", reply_markup=start())
+
+
+
+@bot.message_handler(func=lambda msg: msg.text == "Add_Task")
+def add_task_start(message):
+    msg = bot.send_message(message.chat.id, "Please send the task title:")
+    bot.register_next_step_handler(msg, save_task)
+    
+
 
 
 bot.infinity_polling()
